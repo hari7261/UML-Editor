9cf12db10f6b3f4c80b49d49100fc197f858bb3d
HARIOM KUMAR PANDIT <hariompandit5556@gmail.com>
2025-01-11 14:09:06 +0530

    node  done.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'UML Editor',
+  title: 'UML Editor | hari7261',
   description: 'A simple UML editor built with Next.js',
 }
 
